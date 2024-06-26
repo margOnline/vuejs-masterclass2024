dd8563bfab23c4ebfd032f24d4215c4afb7f49c8
Margo Urey <margo@margonline.co.uk>
2024-06-26 11:08:37 +0100

    Use an IIFE to fetch the project data

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { supabase } from '@/lib/supabaseClient'
+
+const projects = ref()
+
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.error(error)
+
+  projects.value = data
+})()
+</script>
 <template>
   <div>
     <h1>Projects page</h1>
+    <p>{{ projects.length }} projects</p>
     <RouterLink to="/">Home</RouterLink>
   </div>
 </template>
